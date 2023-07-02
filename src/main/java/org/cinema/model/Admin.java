040fb5714ed9fa510bcaa2571795177bc3a19341
mahboobdorali <mahboobdorali@gmail.com>
2023-07-02 05:38:49 +0330

    add option from lombok and delete extra method

--- src/main/java/org/cinema/model/Admin.java
+++ src/main/java/org/cinema/model/Admin.java
@@ -1,20 +1,22 @@
 package org.cinema.model;
 
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@SuperBuilder
+
 class Admin {
     protected String firstName;
     protected String lastName;
     protected String username;
     protected String password;
 
-    public Admin(String firstName,String lastName,String username,String password){
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.username = username;
-        this.password = password;
-    }
-
-
-    public void admin(){
+    public void admin() {
 
     }
 
