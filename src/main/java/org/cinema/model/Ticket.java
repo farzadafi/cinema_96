3e431ce1cb47c11f90156030c4b75b2e81d6bf8c
mahboobdorali <mahboobdorali@gmail.com>
2023-07-02 05:40:17 +0330

    add option from lombok and delete extra method

--- src/main/java/org/cinema/model/Ticket.java
+++ src/main/java/org/cinema/model/Ticket.java
@@ -1,46 +1,22 @@
 package org.cinema.model;
 
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
 import java.sql.Date;
 import java.sql.Time;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@SuperBuilder
 public class Ticket {
-     String cinemaName;
-     String filmName;
-     Date datetime;
-     Time clock;
-     int numberTickets;
-     int  price;
-
-    public Ticket(String cinemaName,String filmName,Date datetime,Time clock,int numberTickets,int price){
-        cinemaName = cinemaName;
-        filmName = filmName;
-        datetime = datetime;
-        clock = clock;
-        numberTickets = numberTickets;
-        price = price;
-    }
-
-    public String getCinemaName() {
-        return cinemaName;
-    }
-
-    public String getFilmName() {
-        return filmName;
-    }
-
-    public Date getDatetime(){
-        return datetime;
-    }
-
-    public Time getClock() {
-        return clock;
-    }
-
-    public int getNumberTickets() {
-        return numberTickets;
-    }
-
-    public int getPrice() {
-        return price;
-    }
+    private String cinemaName;
+    private String filmName;
+    private Date datetime;
+    private Time clock;
+    private int numberTickets;
+    private int price;
 }
--- src/main/java/org/cinema/model/User.java
+++ src/main/java/org/cinema/model/User.java
@@ -1,32 +1,17 @@
 package org.cinema.model;
 
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@SuperBuilder
 public class User {
     protected String firstName;
     protected String lastName;
     protected String username;
     protected String password;
-
-    //::::>
-    public User(String firstName,String lastName,String username,String password){
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.username = username;
-        this.password = password;
-    }
-
-    public String getFirstName(){
-        return firstName;
-    }
-
-    public String getLastName(){
-        return lastName;
-    }
-
-    public String getUsername(){
-        return username;
-    }
-
-    public String getPassword(){
-        return password;
-    }
 }
