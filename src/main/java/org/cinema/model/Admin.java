524882aa7feeb0709c3e5ed18636feb4520a6dd4
Amir Hossein <amir_ahmadi9034@yahoo.com>
2023-07-03 18:43:10 +0430

    deleted @NoArgsConstructor and admin() from Admin

--- src/main/java/org/cinema/model/Admin.java
+++ src/main/java/org/cinema/model/Admin.java
@@ -1,16 +1,11 @@
 package org.cinema.model;
 
 import lombok.*;
-import lombok.experimental.SuperBuilder;
 
 @AllArgsConstructor
-@NoArgsConstructor
 @Getter
 @Setter
 @ToString
 public class Admin extends Person {
-    public void admin() {
-
-    }
 
 }
